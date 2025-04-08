74515300068686c4ac1da0259a13ffbfcd5e019d
NiceK <dan01022001@gmail.com>
2025-04-09 02:48:44 +0300

    Rename LoginScreen to LoginView for consistency purposes, add NavigationService for to swap windows following MVVM, accordingly change LoginView code behind for MVVM coherence, add LoginViewModel with according commands and instance for navigation, add LocalSQLdb with user data(not used yet)

---
+++ Trackery App/Core/INavigationService.cs
@@ -0,0 +1,5 @@
+public interface INavigationService
+{
+    void OpenMainWindow();
+    void CloseLoginView();
+}
---
+++ TrackeryAppSQLpopulationquery.sql
@@ -0,0 +1,20 @@
+﻿create database TrackeryAppDB
+go
+use TrackeryAppDB
+go
+create table [User]
+(
+	Id UNIQUEIDENTIFIER primary Key default NEWID(),
+	Username nvarchar (50) unique not null,
+	[Password] nvarchar (100) not null,
+	[Name]nvarchar (50) not null,
+	LastName nvarchar (50) not null,
+	Email  nvarchar (100) unique not null
+)
+go
+insert into [User] values (NEWID(), 'admin', 'admin','Daniil','Serov', 'cdase001@edu.xamk.fi')
+insert into [User] values (NEWID(), 'Rclick', 'kone','Sasha','Shlyapa', 'sashko@gmail.com')
+insert into [User] values (NEWID(), 'Petu4', 'ursa','Nikita','Tunika', 'dota.ruin@gmail.com')
+go
+
+select *from [User]
