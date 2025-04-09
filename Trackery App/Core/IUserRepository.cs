9a1dea4e21ee30add7ebf0e2762d5032a9322de7
NiceK <dan01022001@gmail.com>
2025-04-10 01:35:52 +0300

    Add AsyncRelay command to handle AsyncLogin with query to sql database, add basic logic for database handling, add UserModel, add Login username password check and its handling with SecureString and other improvements

---
+++ Trackery App/Core/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Trackery_App.Models;
+
+namespace Trackery_App.Core
+{
+    public interface IUserRepository
+    {
+        Task<bool> AuthenticateUserAsync(NetworkCredential credential);
+        bool RegisterUser(UserModel user);
+        bool UpdateUser(UserModel user);
+        bool DeleteUser(int userId);
+        UserModel GetUserById(int userId);
+        UserModel GetUserByUsername(string username);
+        List<UserModel> GetAllUsers();
+    }
+}
